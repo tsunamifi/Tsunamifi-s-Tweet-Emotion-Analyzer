cb34bfd6839e5df270d02213313268e95e5a57b5
tsunamifi <104786105+tsunamifi@users.noreply.github.com>
2022-05-12 10:44:14 -0500

    Update tsuTEAv1.2.1.py

--- tsuTEAv1.2.1.py
+++ tsuTEAv1.2.1.py
@@ -133,7 +133,7 @@ def fetch_tweets():
          stem_tweet = TextBlob(root(parsed_tweet))
          scored_tweet = get_tweet_score(stem_tweet)
          tweets.append((parsed_tweet, clean_tweet, scored_tweet))
-         return tweets
+         return tweetsdf
     except:
       print('Error')
 
