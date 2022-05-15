77635b82e4f7b51763c37a404451cc6e279ee2cd
tsunamifi <104786105+tsunamifi@users.noreply.github.com>
2022-05-14 20:49:52 -0500

    tsuTEAv1.3
    fetches correct amount of tweets :)
    changed headline wording (streamlit formatting)
    default value assigned to streamlit number input (15)
    added metrics visualization for percentages with streamlit metrics
    added loading spinner :)
    cleaning up all comments, making sure they're correct
    removing unneccesary code as well (dead)
    word cloud function live

--- tsuTEAv1.3.py
+++ tsuTEAv1.3.py
@@ -11,16 +11,16 @@ This notebook will allow you to plug in a user OR topic from twitter and determi
 
 Why? for fun, probably.
 
-# Setup
-"""
-
-# Commented out IPython magic to ensure Python compatibility.
 #core setup.
 
 
 ##importing things here!
+
+
+### streamlit is a python web app framework, we're using it for its widgets and cute gui.
 import streamlit as st
-### pandas helps us visualize and sort data, we'll need it if you want to see results.
+
+### pandas helps us sort data, we'll need it if you want to see results.
 import pandas as pd
 
 ### matplot helps us visualize data too.
@@ -39,28 +39,44 @@ from nltk.stem import PorterStemmer
 ### textblob is a text processing library.
 import re
 import tweepy
-#from tweepy.tweet import Tweet
-#from tweepy.user import User
 from tweepy import OAuthHandler
 from textblob import TextBlob
 
-### display is so we can display a dataframe from pandas or something else.
-from IPython.display import display
 
+
+
+# streamlit formatting
 st.set_page_config(layout="wide")
 st.title("Tsunamifi's Twitter Sentiment Analysis Bot")
-st.write("This WebAPP will allow you to plug in a topic from twitter and determine if their tweets are Positive, Negative or Neutral.")
+st.write("This WebAPP will allow you to plug in a topic from twitter and determine if the general discussion is positive, negative or neutral.")
+st.title("Choose Topic on twitter to analyze")
 
-# defining twitter auth setup here!
+with st.form(key='vars'):
+        texti = st.text_input(label='Choose topic')
+        numberi = st.number_input(label= 'How many tweets should we source?', step=1, value=15)
+        submit = st.form_submit_button(label='Submit')
+
+with st.spinner('Collecting Tweets...'):
+    if submit:
+       time.sleep(5)
+    else:
+        pass
+st.success('Done!')
+
+
+
+# inner workings
+
+## defining twitter auth setup here!
 def auth():
 
-    ## keys and token from twitter'
+    ### keys and token from twitter'
     consumer_key = "KLhloeEIOr2de1Cnz7ddcxcmT"
     consumer_secret = "WPQeRE5skCsCfBK8inJSPFTOEFMCUrPGMUyKsi1kjo8xKDaoxQ"
     access_token = "1450493640132923397-NQ1fupgKuJZKZbPsi3gYw9EnngxapO"
     access_token_secret = "eH3IvjkUatJuU7EdYtoSMZckBbIWtPpvYNFUW078VvMK4"
 
-    ## attempting auth...
+    ### attempting auth...
     try:
       auth = OAuthHandler(consumer_key, consumer_secret)
       auth.set_access_token(access_token, access_token_secret)
@@ -70,13 +86,13 @@ def auth():
       print("Error: Authentification Failed, try again?")
       exit(1)
 
-# this will clean unnecessary and maybe complicated things out of a tweet
-# like links or #'s 
+## this will clean unnecessary and maybe complicated things out of a tweet
+## like links or #'s 
 def cleanup(text):
 
-     ## replaces all letters and numbers associated with chars like "\/:"
-     ## (which are chars used in links) with spaces which removes them.
-     ## we're also tokenizing each word here
+     ### replaces all letters and numbers associated with chars like "\/:"
+     ### (which are chars used in links) with spaces which removes them.
+     ### we're also tokenizing each word here
       
       text = re.sub('(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)', ' ', text)
   
@@ -86,8 +102,8 @@ def cleanup(text):
       text = ' '.join(text)
       return text
 
-# here we're getting rid of parts of words that dont mean anything
-# in sentiment analysis so we'll end up with just scoring rootwords
+## here we're getting rid of parts of words that dont mean anything
+## in sentiment analysis so we'll end up with just scoring rootwords
 def root(text):
 
   porter = PorterStemmer()
@@ -98,10 +114,10 @@ def root(text):
   return " ".join(root_sentence)    
     
     
-# lets find out the cleaned tweets' emotion!
+## lets find out the cleaned tweets' general emotion!
 def get_tweet_score(analysis):
 
-    ##then scores the tweet
+    ###"scoring" tweet
     if analysis.sentiment.polarity > 0:
       return 'positive'
     elif analysis.sentiment.polarity < 0:
@@ -110,7 +126,6 @@ def get_tweet_score(analysis):
       return 'neutral'
 
 
-# inner workings
 ## we're gonna grab this x amount of tweets to parse
 def fetch_tweets(query, count = 50):
 
@@ -118,9 +133,9 @@ def fetch_tweets(query, count = 50):
     ### empty list to hold tweets
     tweets = []  
 
-
-    collected_tweets = api.search(q = query + ' -filter:retweets', count = 100)
-
+    collected_tweets = api.search(q = query + ' -filter:retweets', count = count)
+    
+    ### this is a pipeline to do all our our tweet processing
     for Tweet in collected_tweets:
         parsed_tweet = Tweet.text
         clean_tweet = cleanup(parsed_tweet)
@@ -131,40 +146,35 @@ def fetch_tweets(query, count = 50):
     return tweets
     
 
-st.title("Choose Topic on twitter to analyze")
-#st.write("You're welcome to use both a user and topic but both at the same time are not required, you can use one or the other too if you'd like.")
 
 
 # Take off...
 
-
-with st.form(key='vars'):
-        texti = st.text_input(label='Choose topic')
-        numberi = st.number_input(label= 'How many tweets should we source?', step=1)
-        submit = st.form_submit_button(label='Submit')
-        
+## this is how everything comes into play
 def run():
  tweets = fetch_tweets(query = texti, count = numberi)
 
- ## sort and grab percentages between each type
- ## of tweet with pandas..
- df = pd.DataFrame(tweets, columns= ['tweets', 'clean_tweets', 'result'])
+ ### stuff our collected tweets in to a pandas dataframe
+ ### and also specifying which columns.
+ df = pd.DataFrame(tweets, columns= ['Tweets', 'Result'])
 
  ### dropping duplicate tweets too..
  df = df.drop_duplicates(subset='clean_tweets')
  df.to_csv('tweetbank.csv', index= False)
 
- ptweets = df[df['result'] == 'positive']
+ ptweets = df[df['Result'] == 'positive']
  posper = (100*len(ptweets)/len(tweets))
- st.write(f'Positive tweets {posper} %')
   
- ntweets = df[df['result'] == 'negative']
+ ntweets = df[df['Result'] == 'negative']
  negper = (100*len(ntweets)/len(tweets))
- st.write(f'Negative tweets {negper} %')
        
  nuper = (100 - posper - negper)
- st.write(f'Neutral tweets {nuper} %')
-   
+ 
+ st.write("Here's the overall climate concerning {texti}")
+ col1, col2, col3 = st.columns(3)
+ col1.metric("Positive Tweets", "{posper}%")
+ col2.metric("Negative Tweets", "{negper}%")
+ col3.metric("Neutral Tweets", "{nuper}%") 
  st.dataframe(df)
 
  wcloud = st.checkbox(label='Generate word cloud')
@@ -172,15 +182,15 @@ def run():
  twt = " ".join(df['clean_tweets'])
  wordcloud = WordCloud(stopwords=STOPWORDS, background_color='black', width=2500, height=2000).generate(twt)
 
- if wcloud:
-   plt.show()
-   st.pyplot(wordcloud)
- else:
-    pass
+if wcloud:
+      plt.show()
+      plt.figure(1,figsize=(8, 8))
+      plt.axis('off')
+      plt.imshow(wordcloud)
+      st.pyplot(wordcloud)
+    else:
+      pass
 
- plt.figure(1,figsize=(8, 8))
- plt.axis('off')
- plt.imshow(wordcloud)
 
 if submit:
     run()
